5b913331d5c2c82decbe0eba4b49f4bd06caf4e1
NetherDragon99 <netherdragon114@gmail.com>
2024-12-19 02:51:06 +0200

    cart menue
    making cart menu main base and json file for api

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,12 +1,25 @@
 let cart=[];
-const counter=document.createElement('div');
+const counter=document.createElement('div'),
+  counterClone=counter.cloneNode(true),
+  counterContainer=document.querySelector('nav>div:nth-child(3)>span:nth-child(4)'),
+  counterContainerList=document.querySelector('.sidebar>div:nth-child(3)>span:nth-child(4)'),
+  cartMenue=document.createElement('div');
+
+//on load
 document.addEventListener('DOMContentLoaded',()=>{
-  const counterContainer=document.querySelector('nav>div:nth-child(3)>span:nth-child(4)');
+  
+  //add counter
   counter.classList.add('counter');
+  counterClone.classList.add('counter');
   counterContainer.appendChild(counter);
+  counterContainerList.appendChild(counterClone);
   cart=JSON.parse(localStorage.getItem('cart')) || [];
-  ShowCartCounter()
-});
+  ShowCartCounter();
+
+  //counter menue
+  document.querySelector('header').appendChild(cartMenue);
+  cartMenue.classList.add('cartMenue');
+})
 const atc=document.querySelectorAll('.atc');
 atc.forEach((button)=>{
   button.addEventListener('click',()=>{
@@ -18,13 +31,24 @@ atc.forEach((button)=>{
     localStorage.setItem('cart',JSON.stringify(cart));
     counter.textContent=cart.length;
     ShowCartCounter()
-  });
-});
+  })
+})
 function ShowCartCounter(){
   if (cart.length===0){
     counter.style.display='none';
+    counterClone.style.display='none';
   }else{
     counter.style.display='block';
     counter.textContent=cart.length;
+    counterClone.style.display='block';
+    counterClone.textContent=cart.length;
   }
 }
+
+counterContainer.addEventListener('click',()=>{
+  if(cartMenue.classList.contains('on')){
+    cartMenue.classList.remove('on');
+  }else{
+    cartMenue.classList.add('on');
+  }
+})
